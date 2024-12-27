4df4e66214aad2debbf5b6dee922fe00f2c2666d
AHQ Miness <ahqsecret@gmail.com>
2024-12-27 22:41:50 +0530

    feat

--- lead/src/docs.rs
+++ lead/src/docs.rs
@@ -1,5 +1,8 @@
 use std::env;
 
 pub fn run_docs() {
-  env::var("LEAD_HOME");
+  let home = env::var("LEAD_HOME").expect("Broken Lead Installation, LEAD_HOME is necessary");
+  let version = env!("CARGO_PKG_VERSION");
+
+  println!("Home: {home}, Version {version}");
 }
--- lead/src/main.rs
+++ lead/src/main.rs
@@ -1,5 +1,29 @@
-mod docs;
+use std::env::args;
 
+mod docs;
+mod app;
+
+/// The main entry point of the program.
+///
+/// This function will be called when the program starts, and is where program execution begins.
+///
 fn main() {
-    println!("Hello, world!");
+    println!("⚠️ Under Construction ⚠️");
+    let args: Vec<String> = args().collect();
+
+    let cmd0: &str = &args[1];
+
+    match cmd0 {
+        "" | "run" => {
+
+        }
+        "docs" => {
+            docs::run_docs();
+        }
+        e => {
+            if e != "help" {
+                println!("Unknown command: {}", e);
+            }
+        }
+    }
 }
