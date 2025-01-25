536ecab90c348b6877faedd5976d862f713d8ea1
Akshanabha Chakraborty <ahqsecret@gmail.com>
2025-01-25 22:46:45 +0530

    Fix stuff

--- avd_strap.sh
+++ avd_strap.sh
@@ -2,7 +2,7 @@
 
 tmp=$(mktemp -d 2>/dev/null || mktemp -d -t 'mytmpdir')
 
-curl -fsSL https://raw.githubusercontent.com/leadlang/lead/refs/heads/main/leadman_unix_avd.sh -o "$tmp/inst.sh"
+curl -fsSL https://leadlang.github.io/install_advanced.sh -o "$tmp/inst.sh"
 chmod +x "$tmp/inst.sh"
 sh "$tmp/inst.sh"
 
